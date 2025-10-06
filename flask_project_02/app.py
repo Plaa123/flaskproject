c92ada9590d83c786a5e6df2ae5c3aada61d249a
zhu <1034308821@qq.com>
2025-10-06 13:07:29 +0800

    创建项目

--- flask_project_02/app.py
+++ flask_project_02/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify
 
 app = Flask(__name__)
 
@@ -11,8 +11,9 @@ def index():
 #     返回字符串
 #     return '<b>Flask Index </b>'
 # 模版渲染
-    return render_template('index.html')
-
+    return render_template('index.html',name="法外狂徒张三")
+#   json
+#     return jsonify({'name':'Tom','age':25})
 if __name__ == '__main__':
-    app.run(debug=True, port = 5001 )
+    app.run(debug=True, port = 5001)
 
