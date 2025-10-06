ac0db449505a8881ec33fa36b24c56fe59171c27
zhu <1034308821@qq.com>
2025-10-06 13:17:54 +0800

    项目拆分

--- flask_project_02 模版渲染/app.py
+++ flask_project_02 模版渲染/app.py
@@ -8,12 +8,12 @@ def home():
 # 模版渲染
 @app.route('/index/')
 def index():
-#     返回字符串
-#     return '<b>Flask Index </b>'
-# 模版渲染
-    return render_template('index.html',name="法外狂徒张三")
-#   json
-#     return jsonify({'name':'Tom','age':25})
+#       返回字符串
+#       return '<b>Flask Index </b>'
+#       模版渲染
+        return render_template('index.html',name="法外狂徒张三")
+#       json
+#       return jsonify({'name':'Tom','age':25})
 if __name__ == '__main__':
     app.run(debug=True, port = 5001)
 
